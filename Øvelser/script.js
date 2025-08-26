dc9dd3876a2272397742ef2449d66c41eaf7a05d
maen0002 <maen0002@stud.kea.dk>
2025-08-26 14:08:58 +0200

    create exercise lib

---
+++ Øvelser/script.js
@@ -0,0 +1,54 @@
+/* console.log("Tilfældigt tal " + Math.floor(Math.random() * 100));
+document.querySelector("#RandomNumber").innerHTML = "Tilfældigt tal " + Math.floor(Math.random() * 100);
+
+let RandomNum = Math.floor(Math.random() * 100);
+console.log(RandomNum);
+
+const button = document.getElementById("submitGuess");
+button.addEventListener("click", checkGuess);
+
+function checkGuess() {
+  document.querySelector("#Wrong").innerHTML = "";
+  document.querySelector("#Correct").innerHTML = "";
+  let GuessNum = document.getElementById("numberInput").value;
+  GuessNum = Number(GuessNum);
+  console.log(GuessNum);
+  if (GuessNum === RandomNum) {
+    console.log("Correct Guess!");
+    document.querySelector("#Correct").innerHTML = "Du gættede rigtigt tallet var " + RandomNum;
+    document.querySelector("#Correct").classList.add("winner");
+  } else if (GuessNum > RandomNum) {
+    console.log("Wrong Guess!");
+    document.querySelector("#Wrong").innerHTML = "Du gættede forkert dit tal var for højt";
+  } else {
+    console.log("Wrong Guess!");
+    document.querySelector("#Wrong").innerHTML = "Du gættede forkert dit tal var for lavt";
+  }
+}
+  */
+
+// Herunder er hvordan vi lavede den i klassen
+
+"use strict";
+
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+const h2 = document.querySelector("h2");
+
+const randomNum = Math.floor(Math.random() * 100);
+
+btn.addEventListener("click", klik_btn);
+function klik_btn() {
+  let brugerGuess = input.value;
+  let feedbacktext;
+  console.log("Klik", randomNum);
+  console.log(brugerGuess);
+  if (brugerGuess < randomNum) {
+    feedbacktext = "Du gættede for lavt";
+  } else if (brugerGuess > randomNum) {
+    feedbacktext = "Du gættede for højt";
+  } else {
+    feedbacktext = "Du gættede rigtigt";
+  }
+  h2.textContent = feedbacktext;
+}
