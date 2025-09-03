20fc695061699ab5b84c53955e8b664039a78986
maen0002 <maen0002@stud.kea.dk>
2025-09-03 21:24:48 +0200

    filter

--- filter_oevelse_starter_files_table/script.js
+++ filter_oevelse_starter_files_table/script.js
@@ -11,18 +11,57 @@ const vehicles = [
   { type: "Løbehjul", passengers: 1, isElectric: true },
 ];
 const tbodyPointer = document.querySelector("tbody");
-showTheseVehicles(vehicles);
 
 function showTheseVehicles(arr) {
+  tbodyPointer.innerHTML = "";
   arr.forEach((each) => {
     tbodyPointer.innerHTML += `<tr>
-  <td>${each.type}</td>
-  <td>${each.fuel}</td>
-  <td>${each.passengers}</td> 
-  <td>${each.stops}</td>
-  <td>${each.ownedBy}</td>
-  <td>${each.isElectric}</td>
-  <td>${each.isTandem}</td>
+  <td>${isUndefined(each.type)}</td>
+  <td>${isUndefined(each.fuel)}</td>
+  <td>${isUndefined(each.passengers)}</td> 
+  <td>${isUndefined(each.stops)}</td>
+  <td>${isUndefined(each.ownedBy)}</td>
+  <td>${isUndefined(each.isElectric)}</td>
+  <td>${isUndefined(each.isTandem)}</td>
 </tr>`;
   });
 }
+
+function isUndefined(val) {
+  return val ? val : "";
+}
+
+document.querySelectorAll("button").forEach((knap) => knap.addEventListener("click", showFiltered));
+
+function showFiltered() {
+  console.log("klik");
+  const filter = this.dataset.filter;
+  if (filter === "alle") {
+    showTheseVehicles(vehicles);
+  } else if (filter === "eldrevne") {
+    showTheseVehicles(electricVehicles);
+  } else if (filter === "over2") {
+    showTheseVehicles(manyseater);
+  } else if (filter === "eldrevneJonas") {
+    showTheseVehicles(jonas);
+  } else if (filter === "rugbrød") {
+    showTheseVehicles(ryebread);
+  }
+}
+
+const electricVehicles = vehicles.filter((vehicle) => vehicle.isElectric === true);
+// showTheseVehicles(electricVehicles);
+
+const manyseater = vehicles.filter((vehicle) => vehicle.passengers > 2);
+// showTheseVehicles(manyseater);
+
+const jonas = vehicles.filter((vehicle) => vehicle.ownedBy === "Jonas" && vehicle.isElectric === true);
+// showTheseVehicles(jonas);
+
+function ryebreadmore(vehicle) {
+  if (vehicle.fuel === "Rugbrød" && vehicle.passengers > 1) {
+    return true;
+  }
+}
+const ryebread = vehicles.filter(ryebreadmore);
+// showTheseVehicles(ryebread);
